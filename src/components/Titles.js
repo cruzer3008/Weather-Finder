6bd013642a5c8c7306500446f0f97ae1f30d571b
Yash Khakhar <yashpravina@gmail.com>
2018-12-01 11:34:55 +0530

    First Commit

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Titles extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Weather finder</h1>
+                <p>Find out the current weather conditions at your place</p>
+            </div>
+        )
+    }
+}
+
+export default Titles;
